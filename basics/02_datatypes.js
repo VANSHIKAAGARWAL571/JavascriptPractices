cda8412f076b41bbeddb7c5120d8503105b3eda0
Vanshika Agarwal <77267560+VANSHIKAAGARWAL571@users.noreply.github.com>
2023-12-27 15:04:11 +0000

    datatypes understanding basics

---
+++ basics/02_datatypes.js
@@ -0,0 +1,24 @@
+"use strict" // treat all js code as newer version
+// alert (3+3) //we are using node js and not browser
+// console.log(3+3) console.log("dff") readability should be there (error) 
+let name = "vanshika"//string
+let age= 21 //number
+let isLoggedIn= true //boolean
+let state; //undefined
+//primitive data types
+//big int
+//string => ""
+//boolean => true/ false
+//null=> standalone value special type khali hai undefined  nhi hai empty hai
+//undefined=> abhi value assign nhi hui hai
+//symbol=> uniqueness 
+
+
+//Objects
+
+console.log(typeof "vanshika");
+console.log(typeof age)
+console.log(typeof 45.67);
+console.log(typeof null); //but this is an object type
+console.log(typeof undefined); //to ye to apne me hi ek type hua
+
