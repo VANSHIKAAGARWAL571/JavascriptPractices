18a9928ca05a26dee0c542cbccaf726f383ee183
Vanshika Agarwal <77267560+VANSHIKAAGARWAL571@users.noreply.github.com>
2023-12-28 16:24:41 +0000

    Memory

--- basics/DatatypesSummary.js
+++ basics/DatatypesSummary.js
@@ -31,3 +31,23 @@ console.log(typeof(myfunc))
 // Arrays: Object
 // Object: Object
 // Functions: function/ function object
+
+
+
+//++++++++++++++++++++Memory++++++++++++++++++++++++//
+//Stack(Primitive) copy is sent so original value remain intact , Heap(NonPrimitive) direct reference is given so original value gets changed
+let anothername="Vanshika";
+let name1= anothername;
+name1= "Radha"
+console.log(anothername)
+console.log(name1) //Primitive
+let user1={
+    email:"google@gmail.com",
+    upiid: "user@yid",
+}
+let user2= user1;
+user2.email="vanshika@gmail.com"
+console.log(user1.email)
+console.log(user2.email) //NonPrimitive
+
+
