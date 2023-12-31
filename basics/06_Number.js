65bf4cc73e8ac07ad50c9e9e2d7807a8e5afff50
Vanshika Agarwal <77267560+VANSHIKAAGARWAL571@users.noreply.github.com>
2023-12-31 07:08:43 +0000

    Math&Num

---
+++ basics/06_Number.js
@@ -0,0 +1,27 @@
+const score= 400;
+console.log(score);
+const balance = new Number(100)
+console.log(balance);
+console.log(balance.toString().length);
+console.log(balance.toFixed(2))
+const otherNumber =1123.9866
+console.log(otherNumber.toPrecision(3))
+const hundreds = 10000000
+console.log(hundreds.toLocaleString());
+console.log(hundreds.toLocaleString("en-IN"))
+//++++++++++++++++MATHS+++++++++++++++++++//
+
+
+// console.log(Math);
+// console.log (Math.abs(-4.578))
+// console.log(Math.round(4.5))
+// console.log(Math.ceil(4.7))
+// console.log(Math.floor(4.5))
+//  console.log(Math.min(4.6,6.7));
+console.log(Math.random());
+console.log(Math.floor(Math.random()*10)+1)
+const min= 10
+const max= 20
+console.log(Math.floor(Math.random() * (max-min+1))+ min)
+
+
